21d27566a296442ba94eeb50c008eaf6932a0607
michael69zen <145418799+michael69zen@users.noreply.github.com>
2024-01-14 09:41:32 -0500

    Add files via upload

---
+++ OpersdoresLogicos.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;
+    bool respuesta;
+
+    a=5;
+    b=10;
+    c=15;
+    d=20;
+
+    //&& , || , !
+    respuesta = b>a;
+    cout << respuesta;
+
+    return 0;
+}
