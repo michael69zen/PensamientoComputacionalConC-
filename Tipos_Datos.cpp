87b2b6ac307db4c78019d1bfb1b9a6803affb731
michael69zen <145418799+michael69zen@users.noreply.github.com>
2024-01-14 08:58:56 -0500

    building

---
+++ Tipos_Datos.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+//PENSAMIENTO_COMPUTACIONAL
+//Ing. Miguel Romilio Aceituno Rojo
+//Est. Cristhian Michael Jallo Paredes
+//Cod. 236543
+
+//TIPOS DE DATOS
+int main()
+{
+    //declarar numeros enteros
+    int entero=69;
+    cout<<entero<<endl;
+
+    //declarar numeros con punto decimal con float(5decimales)
+    float flotante=3.14166126;
+    cout<<flotante<<endl;
+
+    //declarar numeros con punto decimal con double
+    double doble=3.14166996;
+    cout<<doble<<endl;
+
+    //declarar letra char
+    char letra = 'C';
+    cout<<letra<<endl;
+
+    //declarar un dato booleano 0 y 1
+    bool booleano=true;
+    cout <<booleano<<endl;
+
+    return 0;
+}
